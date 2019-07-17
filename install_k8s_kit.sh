34c7cc7707a5f82394c93fe21492f4abcf3eebf9
jprdonnelly <jprdonnelly@gmail.com>
2019-07-17 13:52:33 -0400

    Update install_k8s_kit.sh

--- install_k8s_kit.sh
+++ install_k8s_kit.sh
@@ -1,37 +1,21 @@
-#!/bin/bash
+!/bin/bash
+
 kubectl create clusterrolebinding default-admin --clusterrole cluster-admin --serviceaccount=default:default
-#
 kubectl apply -f https://raw.githubusercontent.com/jprdonnelly/kubernetes-cluster/master/metallb/metallb.yaml
-#
 kubectl apply -f https://raw.githubusercontent.com/jprdonnelly/kubernetes-cluster/master/metallb/layer-2.yaml
-#
 kubectl label nodes k8s-node1 role=nfs
-#
 kubectl apply -f https://raw.githubusercontent.com/jprdonnelly/kubernetes-cluster/master/nfs-provisioner/nfs-deployment.yaml
-#
 kubectl apply -f https://raw.githubusercontent.com/jprdonnelly/kubernetes-cluster/master/nfs-provisioner/nfs-class.yaml
-#
 kubectl patch storageclass nfs-dynamic -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
-#
-kubectl apply -f https://raw.githubusercontent.com/jprdonnelly/kubernetes-cluster/master/qseok/nfs-vol-pvc.yaml
-#
-rm -f /tmp/get_helm.sh
-wget -O /tmp/get_helm.sh https://git.io/get_helm.sh
-chmod +x /tmp/get_helm.sh
-/tmp/get_helm.sh --version v2.13.1
-#
+curl -LO https://git.io/get_helm.sh
+chmod +x get_helm.sh
+./get_helm.sh --version v2.13.1
 kubectl apply -f https://raw.githubusercontent.com/jprdonnelly/kubernetes-cluster/master/qseok/rbac-config.yaml
-#
-#
 helm init --service-account tiller
-sleep 20
 helm repo add qlik https://qlik.bintray.com/stable
+wget https://raw.githubusercontent.com/jprdonnelly/kubernetes-cluster/master/qseok/qseok-values.yaml
 helm install -n qliksense-init qlik/qliksense-init
-#
-# wget https://raw.githubusercontent.com/jprdonnelly/kubernetes-cluster/master/qseok/qseok-values.yaml
-#
-echo
-echo "*** Please ensure to edit qseok-values.yaml to match your IdP configuration. ***"
-echo
-helm install -n qseok qlik/qliksense -f ./qseok-values.yaml
-#
+helm install -n qliksense qlik/qliksense -f ./qseok-values.yaml
+vim ./qseok-values.yaml
+helm upgrade --install qliksense qlik/qliksense -f ./qseok-values.yaml
+
